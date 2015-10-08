13b7d6feda24a738e34ef00cc3324c7595c1ca93
SkinnyFatMan <SkinnyFatMan@gmail.com>
2015-10-08 11:52:50 -1000

    dashboard/login url redirect working

--- client/views/template/login.js
+++ client/views/template/login.js
@@ -1,1 +1,13 @@
 console.log('js working')
+
+
+
+
+Tracker.autorun(function() {
+  if (Meteor.user() !== null) {
+    Router.go('/dashboard');
+  }
+  if (Meteor.user() === null) {
+    Router.go('/');
+  }
+});
