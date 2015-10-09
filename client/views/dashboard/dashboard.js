f11c04077f41c53bccb51965001ab1491477afb3
SkinnyFatMan <SkinnyFatMan@gmail.com>
2015-10-09 11:13:03 -1000

    Got the KANBUTT working

--- client/views/dashboard/dashboard.js
+++ client/views/dashboard/dashboard.js
@@ -1,51 +1,64 @@
-  Template.todos.helpers({
-    getAllAssignedItems : function() {
-      return Items.find({progress: 1}).fetch();
-    },
-    getAllTodoItems : function() {
-      return Items.find({progress: 2}).fetch();
-    },
-    getAllDoneItems : function() {
-      return Items.find({progress: 3}).fetch();
-    }
-  });
-
-  Template.addTodo.events({
-    'submit form' : function(event) {
-      console.log('submitted');
-      event.preventDefault();
-      var todoName = $('[name="todoName"]').val();
-      var todoDescription = $('[name="todoDescription"]').val();
-      Items.insert({
-        name : todoName,
-        description : todoDescription,
-        completed : false,
-        createdAt : new Date(),
-        progress : 1
-
-      });
-      $('[name="todoName"]').val('');
-      $('[name="todoDescription"]').val('');
-      // getDaItems();
-      // binding items to new Items
-      // this.items = getDaItems();
-    }
-  });
-
-  function getDaItems(prog) {
-    var collection = Items.find({progress: prog}).fetch();
-    console.log(collection);
-    return collection;
+Template.todos.helpers({
+  getAllAssignedItems : function() {
+    return Items.find({ progress : 1 });
   }
+});
+
+Template.todosinprog.helpers({
+  getAllTodoItems : function() {
+    return Items.find({ progress : 2 });
+  }
+});
+
+Template.todosdone.helpers({
+  getAllDoneItems : function() {
+    return Items.find({ progress : 3 });
+  }
+});
+
+Template.addTodo.events({
+  'submit form' : function(event) {
+    console.log('submitted');
+    event.preventDefault();
+    var todoName = $('[name="todoName"]').val();
+    var todoDescription = $('[name="todoDescription"]').val();
+    Items.insert({
+      name : todoName,
+      description : todoDescription,
+      completed : false,
+      createdAt : new Date(),
+      progress : 1
+
+    });
+    $('[name="todoName"]').val('');
+    $('[name="todoDescription"]').val('');
+    // getDaItems();
+    // binding items to new Items
+    // this.items = getDaItems();
+  }
+});
+
+function getDaItems(prog) {
+  var collection = Items.find({progress: prog}).fetch();
+  console.log(collection);
+  return collection;
+}
 
 
-  Template.todoItem.events({
-    'click .delete-todo' : function() {
-      event.preventDefault();
-      var documentId = this._id;
-      // var confirm = window.confirm('Delete this task?');
-      // if (confirm) {
-        Items.remove({ _id : documentId });
-      // }
-    }
-  });
+Template.todoItem.events({
+  'click .delete-todo' : function() {
+    event.preventDefault();
+    var documentId = this._id;
+    // var confirm = window.confirm('Delete this task?');
+    // if (confirm) {
+    Items.remove({ _id : documentId });
+    // }
+  },
+  'click .progress-todo' : function() {
+    console.log(this);
+    event.preventDefault();
+    var documentId = this._id;
+    var todoprogress = this.progress + 1;
+    Items.update({ _id : documentId }, { $set : { progress : todoprogress } } );
+  }
+});
