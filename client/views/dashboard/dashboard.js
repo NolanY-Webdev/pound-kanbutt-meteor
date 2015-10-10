934e278d67f9adb1c851d9772009747e7d8e3f18
SkinnyFatMan <SkinnyFatMan@gmail.com>
2015-10-09 19:01:26 -1000

    Changed login

--- client/views/dashboard/dashboard.js
+++ client/views/dashboard/dashboard.js
@@ -70,3 +70,31 @@ Template.button.events({
     document.body.innerHTML = "YOU WERE WARNED";
   }
 })
+
+Template.login.events({
+  'submit form': function(event) {
+    event.preventDefault();
+    var emailVar = event.target.loginEmail.value;
+    var passwordVar = event.target.loginPassword.value;
+    Meteor.loginWithPassword(emailVar, passwordVar);
+  }
+});
+
+Template.register.events({
+  'submit form': function(event) {
+    event.preventDefault();
+    var emailVar = event.target.registerEmail.value;
+    var passwordVar = event.target.registerPassword.value;
+    Accounts.createUser({
+      email: emailVar,
+      password: passwordVar
+    });
+  }
+});
+
+Template.dashboard.events({
+  'click .logout': function(event){
+    event.preventDefault();
+    Meteor.logout();
+  }
+});
