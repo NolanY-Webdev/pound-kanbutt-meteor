7ac3e87783b6e00420786282ceb06a17dc046cfa
SkinnyFatMan <SkinnyFatMan@gmail.com>
2015-10-09 19:04:31 -1000

    removed more old unnecessary files

--- client/router.js
+++ client/router.js
@@ -3,8 +3,8 @@ Router.configure({
 });
 
 Router.map(function() {
-  this.route('/main', {path:'/'});
-  this.route('/dashboard', {path:'/dashboard'});
+  this.route('/dashboard', {path:'/'});
+  // this.route('/dashboard', {path:'/dashboard'});
   this.route('/profile/edit', {path:'/profile/edit'});
   this.route('/dashboardLayout', {path:'/dashboardLayout'});
 })
